a6144a9d6854f2da595395ad096a892dd76aefe1
Nifer Kilakila <nifercritter@gmail.com>
2022-05-28 21:30:39 -0700

    funtioning planner with polished css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,40 @@
-var today = moment();
+// Global Variables
+let today = moment();
+let now = moment().hour();
+
+// Prints current date/time under header
 $("#currentDay").text(today.format("dddd, MMMM Do YYYY, h:mm"));
 
-var now = moment().format('HH:mm');
-console.log(now)
+// Runs scheduleLoad on ready
+$(document).ready(scheduleLoad);
 
-console.log(moment('8:01', "HH:mm").isBetween('8:00', '8:59', "HH:mm", "HH:mm"));
+// Set colors of hour slots, and inputs local storage.
+function scheduleLoad() {
+    for (i = 8; i <= 17; i++) {
+        if (i < now) {
+            $("#text" + i).removeClass('future').removeClass('present').addClass("past");
+        } else if (i > now) {
+            $("#text" + i).removeClass('present').removeClass('past').addClass("future");
+        } else {
+            $("#text" + i).removeClass('future').removeClass('past').addClass('present');
+        }
+        $("#text" + i).text(localStorage.getItem("text" + i))
+    }
+}
 
-// let amBox8El = $('#amBox8')
+// Function to check every 15 mins to update colors
+setInterval(scheduleLoad, 1000 * 60);
 
+// All save buttons put input into local storage onclick.
+$('.saveBtn').on("click", function (event) {
+    console.log(event);
+    var keyName = $(this).siblings('.entryHere').attr('id');
+    var textValue = $(this).siblings('.entryHere').val();
+    localStorage.setItem(keyName, textValue);
+})
 
-// Check to see all timeBoxes are between their respe tive times
-// If yes = green CSS, If Before = RED, If after
-
-
-
-
-
+// Bonus: Clear local storage button
+$('#clearStorage').on("click", function () {
+    localStorage.clear();
+    scheduleLoad();
+})
